01dbc89de4904651952b6821b8f9ac55563aeba8
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2023-03-29 17:36:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const narbar=document.querySelector(".list-item");
+
+document.querySelector(".toggle").addEventListener("click",function(){
+    narbar.classList.toggle("navbar-toggle");
+})
+
+// bar.addEventListener("click",function(){
+//     alert("hi");
+//   //  
+// })
+
+
+// $('.toggle').click(function () {
+//     "use strict";
+//     $('div a').slideToggle();
+// });
+
+
+
+// $(window).resize(function () {
+//     "use strict";
+//     if ($(window).width() > 300) {
+//         $('div a').removeAttr('style');
+//     }
+// });
